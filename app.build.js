4df4c6d1bd983bfceee047eb03b43f75db0945c4
Marlin Forbes <marlinf@datashaman.com>
2013-04-15 22:16:09 +0200

    WIP

---
+++ app.build.js
@@ -0,0 +1,20 @@
+({
+    mainConfigFile: "static/main.js",
+
+    appDir: "./static/",
+    dir: "./build/",
+    baseUrl: "./",
+
+    skipDirOptimize: true,
+
+    optimize: "uglify2",
+    optimizeCss: "standard.keepComments",
+
+    stubModules: ["cs"],
+    modules: [
+        {
+            name: "main",
+            exclude: ["coffee-script"]
+        }
+    ]
+})
