08bb5cd0d2531307f9b2e484aa4eee54211181b9
Marlin Forbes <marlinf@datashaman.com>
2013-04-18 09:24:55 +0200

    Moves

--- apps/flagel/config.py
+++ apps/flagel/config.py
@@ -3,9 +3,9 @@ import yaml
 
 
 APP_ENV = os.getenv('APP_ENV', 'development')
-APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+APP_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-config_filepath = os.path.join(APP_ROOT, 'config', 'config.yaml')
+config_filepath = os.path.join(APP_ROOT, 'config', 'flagel.yaml')
 config = None
 
 with open(config_filepath, 'r') as file:
